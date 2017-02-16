e93a8d8510a66090ea5062b3df674ca84f98d354
Vo Quang Hoa <Vo Quang Hoa>
2017-02-16 23:34:42 +0700

    Add Dw

---
+++ DwList/Program.cs
@@ -0,0 +1,89 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DwList
+{
+    class Href
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public Href(HtmlNode node)
+        {
+            Text = node.InnerText;
+            Url = node.Attributes["href"].Value;
+        }
+    }
+
+    class Program
+    {
+        private static string localDataPath = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateFolder(localDataPath);
+                var url = "http://www.dw.com/de/viele-k%C3%B6che-verderben-den-brei/l-36385982";
+                var xpath = "//div[@id=\"bodyContent\"]//ul[@class=\"smallList\"][1]/li/a";
+                var html = new HtmlWeb().Load(url);
+                var nodes = html.DocumentNode.SelectNodes(xpath).Where(x => x.Attributes.Count == 1).ToArray();
+                var links = nodes.Select(x => new Href(x)).ToArray();
+                foreach (var link in links)
+                {
+                    Console.WriteLine($"{link.Text} -- {link.Url}");
+                    loadPage(link);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                
+            }
+            Console.ReadKey();
+        }
+
+        private static void CreateFolder(string folder)
+        {
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+        
+        private static void loadPage(Href href)
+        {
+            var dataPath = Path.Combine(localDataPath, href.Text);
+            CreateFolder(dataPath);
+
+            var textXpath = "//div[@class=\"dkTaskWrapper tab3\"]";
+            var audioXpath = "//div[@class=\"mediaItem\"]/input[@name=\"file_name\"]";
+            var imgXpath = "//div[@class=\"mediaItem\"]/input[@name=\"preview_image\"]";
+            
+            var html = new HtmlWeb().Load("http://www.dw.com" + href.Url);
+            var document = html.DocumentNode;
+
+            var text = document.SelectSingleNode(textXpath).InnerText;
+            var audio = document.SelectSingleNode(audioXpath).Attributes["value"].Value;
+            var img = "http://www.dw.com" + document.SelectSingleNode(imgXpath).Attributes["value"].Value;
+
+            File.WriteAllText(Path.Combine(dataPath, "text.txt"), text);
+            rawDownload(audio, Path.Combine(dataPath, "audio.mp3"));
+            rawDownload(img, Path.Combine(dataPath, "photo.jpg"));
+        }
+
+        private static void rawDownload(string url, string fileName)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(url, fileName);
+            }
+        }
+    }
+}
--- WebCraping/Program.cs
+++ WebCraping/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -17,17 +18,49 @@ namespace WebCraping
             { "&#8211;","-"},
             { "&#8230;", "..." }
         };
+        static string fileLog = Path.Combine(Directory.GetCurrentDirectory(), "Log.txt");
+        static string currentPath = Directory.GetCurrentDirectory();
+
+        static void Log(string text)
+        {
+            File.AppendAllText(fileLog, text + "\n");
+            Console.WriteLine(text);
+        }
 
         static void Main(string[] args)
         {
-            var dataPath = Path.Combine(@"E:\DataDownload", "Data");
-            CreateFolder(dataPath);
+            try
+            {
+                var dataPath = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+                var test = ConfigurationManager.GetSection("test");
+                CreateFolder(dataPath);
 
-            loadCategory(dataPath, "http://slowgerman.com/inhaltsverzeichnis/");
+                loadCategory(dataPath, "http://slowgerman.com/inhaltsverzeichnis/");
+                /*loadCategory(dataPath, "http://slowgerman.com/category/absolute-beginner/");
+                loadCategory(dataPath, "http://slowgerman.com/category//alltag-2/");
+                loadCategory(dataPath, "http://slowgerman.com/category/dialog-2/");
+                loadCategory(dataPath, "http://slowgerman.com/category//essen-trinken/");
+                loadCategory(dataPath, "http://slowgerman.com/category//feiertage/");
+                loadCategory(dataPath, "http://slowgerman.com/category//freundschaft-familie/");
+                loadCategory(dataPath, "http://slowgerman.com/category//geschichte/");
+                loadCategory(dataPath, "http://slowgerman.com/category//gesundheit-soziales/");
+                loadCategory(dataPath, "http://slowgerman.com/category//kunst-und-kultur/");
+                loadCategory(dataPath, "http://slowgerman.com/category//sprache/");
+                loadCategory(dataPath, "http://slowgerman.com/category//verkehr/");
+                */
+                
+            }
+            catch (Exception ex)
+            {
+                Log("Error " + ex.Message);
+                Log(ex.StackTrace);
+            }
         }
 
         private static void loadCategory(string baseDirectory, string url)
         {
+            Log($"Load category url :::\t {url}");
+
             var folderName = Path.Combine(baseDirectory, GetUrlName(url));
             CreateFolder(folderName);
 
@@ -35,26 +68,46 @@ namespace WebCraping
             var nodes = html.DocumentNode.SelectNodes("//ul[@class=\"lcp_catlist\"]/li/a").ToArray();
             foreach (HtmlNode item in nodes)
             {
-                Console.WriteLine(item.Attributes["title"].Value + "  " + item.Attributes["href"].Value);
+                Log(item.Attributes["title"].Value + "  " + item.Attributes["href"].Value);
                 DownloadPage(folderName, item.Attributes["href"].Value);
             }
 
             Console.ReadKey();
         }
 
+        private static string TinyPath(string path)
+        {
+            return path.Remove(0, currentPath.Length);
+        }
+
         private static void DownloadPage(string saveTo, string url)
         {
+            Log($"Load page ::: {url}");
+
             saveTo = Path.Combine(saveTo, GetUrlName(url));
             CreateFolder(saveTo);
 
             if (File.Exists(Path.Combine(saveTo, "done")))
-                return; 
+            {
+                Log("Skip " + TinyPath(saveTo));
+                return;
+            }
 
-            var html = new HtmlWeb().Load(url);
+            try
+            {
+                var html = new HtmlWeb().Load(url);
 
-            loadText(html.DocumentNode, saveTo);
-            loadAudio(html.DocumentNode, saveTo);
-            loadPhoto(html.DocumentNode, saveTo);
+                loadText(html.DocumentNode, saveTo);
+                loadAudio(html.DocumentNode, saveTo);
+                loadPhoto(html.DocumentNode, saveTo);
+
+                File.Create(Path.Combine(saveTo, "done"));
+            }
+            catch(Exception ex)
+            {
+                Log("Error " + ex.Message);
+                Log(ex.StackTrace);
+            }
         }
 
         private static void loadPhoto(HtmlNode htmlNode, string folder)
@@ -100,6 +153,8 @@ namespace WebCraping
         }
         private static void rawDownload(string url, string fileName)
         {
+            Log($"Download {url} to {TinyPath(fileName)}");
+
             using (var client = new WebClient())
             {
                 client.DownloadFile(url, fileName);
@@ -138,6 +193,7 @@ namespace WebCraping
 
             if (content.Contains("&#"))
             {
+                Log("Wrong content" + content);
                 throw new Exception("Sai sai");
             }
 
